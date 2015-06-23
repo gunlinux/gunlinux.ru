f6d0d2187a525574dcc737efaa66bb19d6ee40f3
Vladimir Abiduev <gunlinux@ya.ru>
2015-06-24 02:17:09 +0800

    gulp vhash

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,34 @@
-
 // configuration
 var input = 'pro/static/src/style.css';
 var output = 'pro/static/css/';
-var autoprefixerOptions = { browsers: ['> 1%', 'IE 7'] };
-var atImport = require("postcss-import");
+var autoprefixerOptions = {browsers: ['> 1%', 'IE 7']};
+var atImport = require('postcss-import');
 
-var gulp = require('gulp'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer-core'),
-    nano = require('gulp-cssnano');
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer-core');
+var nano = require('gulp-cssnano');
+var vhash = require('gulp-vhash');
 
 processors = [
-    atImport({path: ["pro/static/sass"]}),
+    atImport({path: ['pro/static/sass']}),
     require('autoprefixer-core'),
     require('postcss-nested'),
     autoprefixer(autoprefixerOptions)
-    /*
-    function(css) {
-        // sans-serif fallback
-        css.eachDecl('font-family', function(decl) {
-            decl.value = decl.value + ', sans-serif';
-        });
-    }, */
 ];
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     return gulp.src(input)
         .pipe(postcss(processors))
         .pipe(nano())
-        .pipe(gulp.dest(output))
+        .pipe(gulp.dest(output));
 });
 
-
-
-
-gulp.task('watch', function() {
-  return gulp
-    // Watch the input folder for change,
-
-    // and run `sass` task when something happens
-    .watch('pro/static/sass/*.sass', ['sass'])
-    // When there is a change,
-    // log a message in the console
-    .on('change', function(event) {
-      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-      gulp.task('sass');
-    });
+gulp.task('hash', function () {
+    return gulp.src(
+        'pro/static/{css,js}/*.{css,js}'
+    )
+    .pipe(vhash('pro/templates/**/*.html'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'hash']);
