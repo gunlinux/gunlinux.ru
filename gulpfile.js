a4753cba04cbaca0db27dadd011f2466a941ed8a
Vladimir Abiduev <gunlinux@ya.ru>
2015-06-27 22:26:18 +0800

    added html linter

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,14 @@ var input = 'pro/static/src/style.css';
 var output = 'pro/static/css/';
 var autoprefixerOptions = {browsers: ['> 1%', 'IE 7']};
 var atImport = require('postcss-import');
-
+var vhash = require('gulp-vhash');
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer-core');
-var nano = require('gulp-cssnano');
 var csslint = require('gulp-csslint');
-var vhash = require('gulp-vhash');
+var download = require('gulp-download');
+var htmllint = require('gulp-htmllint');
+var html5Lint = require('gulp-html5-lint');
 
 processors = [
     atImport({path: ['pro/static/sass']}),
@@ -21,18 +22,28 @@ processors = [
 gulp.task('sass', function () {
     return gulp.src(input)
         .pipe(postcss(processors))
-        //.pipe(nano())
         .pipe(gulp.dest(output));
 });
 
-gulp.task('hash', ['sass'], function () {
-    return gulp.src(
-        'pro/static/{css,js}/*.{css,js}'
-    )
-    .pipe(vhash('pro/templates/*/*.html'));
+var urls = [
+    'http://dev.gunlinux.org'
+];
+
+gulp.task('testprepare', function () {
+    return download(urls)
+        .pipe(gulp.dest('tmp/test/'));
 });
 
+gulp.task('htmllint', ['testprepare'], function () {
+    return gulp.src('tmp/test/*')
+        .pipe(htmllint())
+        .pipe(html5Lint());
+});
 
+gulp.task('hash', ['sass'], function () {
+    return gulp.src('./pro/static/{css,js}/*.css')
+        .pipe(vhash('./pro/templates/**/*.html'));
+});
 
 gulp.task('csstest', ['sass'], function () {
     gulp.src('pro/static/css/style.css')
@@ -46,5 +57,7 @@ gulp.task('csstest', ['sass'], function () {
     }))
     .pipe(csslint.reporter());
 });
+gulp.task('test', ['csstest', 'htmllint']);
+gulp.task('build', ['sass', 'hash']);
+gulp.task('default', ['build', 'test']);
 
-gulp.task('default', ['sass', 'csstest', 'hash']);
