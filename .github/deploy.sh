a4ded9d1093a25bd3b3f5919f9aa08afe7486f88
loki <gunlinux@ya.ru>
2025-07-25 04:12:59 +1000

    deploy migration fix

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -13,6 +13,7 @@ git reset --hard origin/$BRANCH
 # Optionally run build or install commands here if needed
 # e.g., npm install, yarn install for Node.js projects
 /home/loki/.local/bin/uv sync  || exit
+/home/loki/.local/bin/uv run flask db upgrade || exit
 
 # Restart services using sudo
 sudo systemctl restart gunlinux.ru
