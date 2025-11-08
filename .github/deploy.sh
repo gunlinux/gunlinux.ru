3c5b5a610d7d9226cb45662b24101ff99ccae23d
loki <gunlinux@ya.ru>
2025-11-09 06:13:25 +1000

    front deploy

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -12,6 +12,9 @@ git reset --hard origin/$BRANCH
 
 # Optionally run build or install commands here if needed
 # e.g., npm install, yarn install for Node.js projects
+npm install
+npx vite build
+
 /home/loki/.local/bin/uv sync  || exit
 /home/loki/.local/bin/uv run flask db upgrade || exit
 
